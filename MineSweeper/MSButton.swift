d3bfe11d4bb1c229fadc190134b265f31dcd5075
Genki Ishibashi <b0941015@gmail.com>
2015-01-09 10:38:47 +0900

    remove default ViewController and create Model and View

---
+++ MineSweeper/MSButton.swift
@@ -0,0 +1,21 @@
+//
+//  MSButton.swift
+//  MineSweeper
+//
+//  Created by Genki Ishibashi on 2015/01/08.
+//  Copyright (c) 2015年 Genki Ishibashi. All rights reserved.
+//
+
+import UIKit
+
+class MSButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MineSweeper/MSGameFieldView.swift
@@ -0,0 +1,21 @@
+//
+//  MSGameFieldView.swift
+//  MineSweeper
+//
+//  Created by Genki Ishibashi on 2015/01/08.
+//  Copyright (c) 2015年 Genki Ishibashi. All rights reserved.
+//
+
+import UIKit
+
+class MSGameFieldView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
